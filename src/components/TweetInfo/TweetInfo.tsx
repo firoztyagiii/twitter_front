3245c83ca497919f118bc92a9402865921239574
firoztyagiii <firoztyagi1783@gmail.com>
2023-09-22 14:37:12 +0530

    added tweet info

--- src/components/TweetInfo/TweetInfo.tsx
+++ src/components/TweetInfo/TweetInfo.tsx
@@ -1,14 +1,86 @@
 import { useParams } from "react-router-dom";
 import styles from "./TweetInfo.module.css";
 import { useQuery } from "react-query";
+import apiTweetInfo from "../../services/apiTweetInfo";
+import Spinner from "../../UI/Spinner/Spinner";
+
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { AiOutlineComment } from "react-icons/ai";
+import { AiOutlineRetweet } from "react-icons/ai";
+import { AiOutlineShareAlt } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+
+import { GoBookmark } from "react-icons/go";
 
 const TweetInfo = () => {
-  const { username, tweetId } = useParams();
-  const query = useQuery({
-    queryFn: () => {},
+  const { tweetId } = useParams();
+
+  const { isLoading, data } = useQuery({
+    queryFn: async () => apiTweetInfo(tweetId),
     queryKey: ["tweetInfo"],
   });
-  return <div>TWEET INFO</div>;
+
+  console.log(data);
+
+  return (
+    <div className={styles.tweetInfo}>
+      {isLoading && <Spinner></Spinner>}
+      {!isLoading && data && (
+        <div className={styles.tweetInfoUser}>
+          <div className={styles.tweetInfoBack}>
+            <span>
+              <AiOutlineArrowLeft></AiOutlineArrowLeft>
+            </span>
+            <p>Post</p>
+          </div>
+          <div className={styles.tweetInfoUserContent}>
+            <div className={styles.tweetInfoUserContentProfile}>
+              <div className={styles.tweetInfoUserContentImg}>
+                <img src={data.user.image} alt="" />
+              </div>
+              <div>
+                <p className={styles.tweetInfoName}>Tristan tate</p>
+                <p className={styles.tweetInfoUserName}>@tristantate</p>
+              </div>
+            </div>
+            <p className={styles.tweetInfoContentText}>
+              I need to follow more people. Who am I missing out on?
+            </p>
+            <div className={styles.tweetInfoContentTime}>
+              <p className={styles.tweetInfoUserTweetTime}>
+                {Date(data.createAt).substring(0, 10)}
+              </p>
+              <span>
+                <p>1.2M</p>
+                <p className={styles.view}>Views</p>
+              </span>
+            </div>
+            <div className={styles.tweetInfoOpertions}>
+              <div className={styles.tweetInfoOpertion}>
+                <AiOutlineComment></AiOutlineComment>
+                <p className={styles.tweetInfoOperationCount}>98</p>
+              </div>
+              <div className={styles.tweetInfoOpertion}>
+                <AiOutlineRetweet></AiOutlineRetweet>
+                <p className={styles.tweetInfoOperationCount}>122</p>
+              </div>
+              <div className={styles.tweetInfoOpertion}>
+                <AiOutlineHeart></AiOutlineHeart>
+                <p className={styles.tweetInfoOperationCount}>122</p>
+              </div>
+              <div className={styles.tweetInfoOpertion}>
+                <GoBookmark></GoBookmark>
+                <p className={styles.tweetInfoOperationCount}>122</p>
+              </div>
+              <div className={styles.tweetInfoOpertion}>
+                <AiOutlineShareAlt></AiOutlineShareAlt>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TweetInfo;
---
+++ src/services/apiTweetInfo.ts
@@ -0,0 +1,16 @@
+import { HOST } from "../utils/host";
+
+const apiTweetInfo = async (id: string | undefined) => {
+  try {
+    const response = await fetch(`${HOST}/api/v1/tweet/${id}`);
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    const tweet = await response.json();
+    return tweet.data.tweet;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export default apiTweetInfo;
